f28e007ff20666f9ba870b8e83ed6549476aa816
ErickGonzalez <anonymousvi16@gmail.com>
2022-03-24 18:13:19 -0500

    Correccion rutas

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
 // Crear un nuevo usuario
-router.post( '/new/auth', [
+router.post( '/new', [
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
@@ -17,14 +17,14 @@ router.post( '/new/auth', [
 ], crearUsuario );
 
 // Login de usuario
-router.post( '/auth', [
+router.post( '/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').isLength({ min: 6 }),
     validarCampos
 ], loginUsuario );
 
 // Validar y revalidar token
-router.get( '/auth/renew', validarJWT , revalidarToken );
+router.get( '/renew', validarJWT , revalidarToken );
 
 
 
